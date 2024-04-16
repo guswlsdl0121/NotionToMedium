093ec99d64fd7f32e45314d677280735242ae363
hyunjin <guswlsdl0121@gmail.com>
2024-04-16 23:36:01 +0900

    feat: 노션 연결 후, 발행요청 상태의 페이지 마크다운파일 저장 구현

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import configparser
-from notion import fetch_all_database_entries, print_all_entries
+from notion import get_notion_client, verify_notion_login, fetch_pages_to_publish, export_markdown_pages
 
-if __name__ == '__main__':
+def load_config():
+    """config.ini 파일로부터 토큰과 데이터베이스 id를 가져옵니다."""
     config = configparser.ConfigParser()
     config.read('config.ini')
-    integration_token = config['NOTION_CREDENTIAL']['APIKey']
-    database_id = config['NOTION_CREDENTIAL']['DatabaseId']
+    return config['NOTION_CREDENTIAL']['INTEGRATION_TOKEN'], config['NOTION_CREDENTIAL']['DatabaseId']
 
-    all_entries = fetch_all_database_entries(database_id, integration_token)
-    print_all_entries(all_entries)
+def main():
+    notion_token, database_id = load_config()
+    notion = get_notion_client(notion_token)
+    if verify_notion_login(notion):
+        print("API연결이 확인됐습니다.")
+        page_ids = fetch_pages_to_publish(notion, database_id)
+        if page_ids:
+            export_markdown_pages(notion_token, page_ids)
+        else:
+            print("Markdown으로 추출할 이미지가 존재하지 않습니다.")
+    else:
+        print("API연결에 실패했습니다. 토큰을 확인하세요.")
+
+if __name__ == '__main__':
+    main()
--- notion.py
+++ notion.py
@@ -1,37 +1,44 @@
-import requests
-import json
+from notion_client import Client
+from notion_exporter import NotionExporter
 
-def fetch_all_database_entries(database_id, integration_token):
-    all_data = []
-    # 여기서 최신Notion-Version 확인 : https://developers.notion.com/reference/changes-by-version
-    headers = {
-        "Authorization": f"Bearer {integration_token}",
-        "Notion-Version": "2022-06-28",
-        "Content-Type": "application/json"
+def get_notion_client(notion_token):
+    """클라이언트 객체 생성"""
+    return Client(auth=notion_token)
+
+def verify_notion_login(notion):
+    """로그인 확인"""
+    try:
+        notion.users.list()
+        print("Notion에 성공적으로 로그인했습니다.")
+        return True
+    except Exception as e:
+        print(f"Notion에 로그인을 실패하였습니다: {e}")
+        return False
+
+def fetch_pages_to_publish(notion, database_id):
+    """데이터베이스에서 "발행요청" 상태의 페이지를 조회합니다."""
+    query = {
+        "filter": {
+            "property": "상태",
+            "select": {"equals": "발행요청"}
+        }
     }
-    url = f"https://api.notion.com/v1/databases/{database_id}/query"
-    
-    has_more = True
-    start_cursor = None
-    
-    while has_more:
-        if start_cursor:
-            payload = json.dumps({"start_cursor": start_cursor})
-            response = requests.post(url, headers=headers, data=payload)
-        else:
-            response = requests.post(url, headers=headers)
-        
-        if response.status_code == 200:
-            result = response.json()
-            all_data.extend(result['results'])
-            has_more = result.get('has_more', False)
-            start_cursor = result.get('next_cursor', None)
-        else:
-            print("오류 발생:", response.status_code, response.text)
-            break
+    try:
+        response = notion.databases.query(database_id=database_id, **query)
+        page_ids = [page['id'] for page in response['results']]
+        if not page_ids:
+            print("'발행요청'상태의 페이지가 존재하지 않습니다.")
+        return page_ids
+    except Exception as e:
+        print(f"api에 대한 응답을 받아오지 못했습니다: {e}")
+        return []
 
-    return all_data
-
-def print_all_entries(all_data):
-    for entry in all_data:
-        print(json.dumps(entry, ensure_ascii=False, indent=4))
+def export_markdown_pages(notion_token, page_ids):
+    """마크다운 파일을 추출해서 저장합니다."""
+    exporter = NotionExporter(notion_token=notion_token)
+    exported_pages = exporter.export_pages(page_ids=page_ids)
+    for page_id, content in exported_pages.items():
+        filename = f"{page_id}.md"
+        with open(filename, 'w', encoding='utf-8') as md_file:
+            md_file.write(content)
+        print(f"마크다운 파일이 저장되었습니다. {filename}")
